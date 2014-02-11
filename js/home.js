2b032f142ed43aadc9cc8b7901f4bf4809b86ebf
Lee Asher <rleeasher@gmail.com>
2014-02-11 12:30:53 -0700

    added event handlers

--- js/home.js
+++ js/home.js
@@ -3,73 +3,137 @@ $(function(){
 
 
 
-//d3 stuff
-var getData = function(stock) {
-	var index = $.inArray(stock,pluck(marketData.stockList,stock));
-	var arr = [];
-	arr.push(marketData.stockList[0],marketData.stockList[1],marketData.stockList[index]);
-	return arr;
-};
+//d3 stuff getting the data
+	var getData = function(stocks,attr) {
+		var arr = [];
+		var indicies = [];
+		arr.push(
+			{	'name': 		marketData.stockList[0].niceName,
+				'metric':	marketData.stockList[0][attr]},
+			{	'name': 		marketData.stockList[1].niceName,
+				'metric':	marketData.stockList[1][attr]});
+
+		for (var i = 0; i < stocks.length; i++) {
+			var index = $.inArray(stocks[i],pluck(marketData.stockList,'ticker'));
+			arr.push({'name': marketData.stockList[index].niceName,'metric': marketData.stockList[index][attr]})
+		};
+		console.log(arr);
+		
+		return arr;
+	};
+
+// get an individual object back
+	var getStock = function(stock) {
+		var index = $.inArray(stock,pluck(marketData.stockList,'niceName'));
+		return marketData.stockList[index];
+	}
 
 
-var drawGraph = function(arr,attribute) {
+// this draws the graph 
+	var drawGraph = function(stocks,attribute,graphnum) {
 
-	var margin = {top: 20, right: 30, bottom: 30, left: 40},
-	    width = $(document).width()*0.4,
-	    height = $(document).height()*0.3;
+		var data = getData(stocks,attribute);
+		var graph = "."+graphnum;
 
-	var x = d3.scale.ordinal()
-	    .rangeRoundBands([0, width], .1);
+		var margin = {top: 0, right: 30, bottom: 30, left: 40},
+		    width = $(window).width()*0.4,
+		    height = $(window).height()*0.3;
 
-	var y = d3.scale.linear()
-	    .range([height, 0]);
+		var x = d3.scale.ordinal()
+		    .rangeRoundBands([0, width], .1);
 
-	var xAxis = d3.svg.axis()
-	    .scale(x)
-	    .orient("bottom");
+		var y = d3.scale.linear()
+		    .range([height, 0]);
 
-	var yAxis = d3.svg.axis()
-	    .scale(y)
-	    .orient("left")
-	    .ticks(10);
+		var xAxis = d3.svg.axis()
+		    .scale(x)
+		    .orient("bottom");
 
-	var chart = d3.select(".chart")
-	    .attr("width", width + margin.left + margin.right)
-	    .attr("height", height + margin.top + margin.bottom)
-	  .append("g")
-	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		var yAxis = d3.svg.axis()
+		    .scale(y)
+		    .orient("left")
+		    .ticks(10);
 
-	var datax = arr;
-	var datay = 
-	  x.domain(data.map(function(d) { return d.niceName; }));
-	  y.domain([0, d3.max(data, function(d) { return d.attribute; })]);
 
-	  chart.append("g")
-	      .attr("class", "x axis")
-	      .attr("transform", "translate(0," + height + ")")
-	      .call(xAxis);
+		//select the dom element here
+		var chart = d3.select(graph)
+		    .attr("width", width + margin.left + margin.right)
+		    .attr("height", height + margin.top + margin.bottom)
+		  .append("g")
+		    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	  chart.append("g")
-	      .attr("class", "y axis")
-	      .call(yAxis)
-	    .append("text")
-		    .attr("transform", "rotate(-90)")
-		    .attr("y", 6)
-		    .attr("dy", ".71em")
-		    .style("text-anchor", "end")
-		    .text(attribute);
 
-	  chart.selectAll(".bar")
-	      .data(data)
-	    .enter().append("rect")
-	      .attr("class", "bar")
-	      .attr("x", function(d) { return x(d.niceName); })
-	      .attr("y", function(d) { return y(d.attribute); })
-	      .attr("height", function(d) { return height - y(d.attribute); })
-	      .attr("width", x.rangeBand());
-}
+		//set the x an y domain
+		  x.domain(data.map(function(d) { return d.name; }));
+		  y.domain([0, d3.max(data, function(d) { return d.metric; })]);
 
-drawGraph(getData('GOOG'),'Price');
+		  chart.append("g")
+		      .attr("class", "x axis")
+		      .attr("transform", "translate(0," + height + ")")
+		      .call(xAxis);
+
+		  chart.append("g")
+		      .attr("class", "y axis")
+		      .call(yAxis)
+		    .append("text")
+			    .attr("transform", "rotate(-90)")
+			    .attr("y", 6)
+			    .attr("dy", ".71em")
+			    .style("text-anchor", "end")
+			    .text(attribute);
+
+		  chart.selectAll(".bar")
+		      .data(data)
+		    .enter().append("rect")
+		      .attr("class", "bar ")
+		      //added in a data class
+		      .attr("data-class", function(d){
+		      	return d.name;
+		      })
+		      .attr("x", function(d) { return x(d.name); })
+		      .attr("y", function(d) { return y(d.metric); })
+		      .attr("height", function(d) { return height - y(d.metric); })
+		      .attr("width", x.rangeBand());
+	};
+
+	//make a function that can display stats
+	var displayStats = function () {
+		var obj = getStock($(this).data("class"));
+		console.log(obj);
+		var fadeout = 500;
+		var fadein = 500;
+		$('#ticker').fadeOut(fadeout,function(){
+			$(this).text(obj.ticker).fadeIn(fadein);
+		});
+		$('#niceName').fadeOut(fadeout,function(){
+			$(this).text(obj.niceName).fadeIn(fadein);
+		});
+		$('#30').fadeOut(fadeout,function(){
+			$(this).text(obj[30]).fadeIn(fadein);
+		});
+		$('#60').fadeOut(fadeout,function(){
+			$(this).text(obj[60]).fadeIn(fadein);
+		});
+		$('#90').fadeOut(fadeout,function(){
+			$(this).text(obj[90]).fadeIn(fadein);
+		});
+		$('#120').fadeOut(fadeout,function(){
+			$(this).text(obj[120]).fadeIn(fadein);
+		});
+		$('#Price').fadeOut(fadeout,function(){
+			$(this).text(obj.Price).fadeIn(fadein);
+		});
+		$('#PE').fadeOut(fadeout,function(){
+			$(this).text(obj.PE).fadeIn(fadein);
+		});
+		$('#EPS').fadeOut(fadeout,function(){
+			$(this).text(obj.EPS).fadeIn(fadein);
+		});
+		$('#Beta').fadeOut(fadeout,function(){
+			$(this).text(obj.Beta).fadeIn(fadein);
+		});
+
+	};
 
 
 //ui stuff
@@ -77,6 +141,21 @@ drawGraph(getData('GOOG'),'Price');
 		.dropdown({
 		on: 'hover'
 	});
+
+//Event Handlers
+	
+
+
+
+	$(document).on('click','.bar',displayStats);
+
+
+
+	//draw some bs stuff
+	drawGraph(["AAPL","MSFT"],"30","chart1");
+	drawGraph(["GOOG","LNKD"],"60","chart2");
+
+
 
 
 });	
